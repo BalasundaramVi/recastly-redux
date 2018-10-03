03e8d98de1fc74286966a44364335a728838f3aa
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-03 14:09:45 -0700

    Implement action functions changeVideo & List

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,20 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  if (state === null) {
+    return state;
+  }
+  
+  switch (action.type) {
+
+  case 'CHANGE_VIDEO':
+    outputObj.currentVideo = action.video;
+    return outputObj;
+    
+  default:
+    return state;
+  }
 };
 
 export default currentVideoReducer;
